8574a68cdd0d90b3aa44576ec72f95fcf91d81d8
randiltennakoon <randil2016al@gmail.com>
2021-12-27 18:21:27 +0530

    added AddTaskView and done changes

---
+++ MyToDo/Views/AddTaskView.swift
@@ -0,0 +1,20 @@
+//
+//  AddTaskView.swift
+//  MyToDo
+//
+//  Created by Randil Tennakoon on 2021-12-27.
+//
+
+import SwiftUI
+
+struct AddTaskView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct AddTaskView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddTaskView()
+    }
+}
