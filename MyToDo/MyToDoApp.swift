be8b82b682381e1439555b37d6c3e3e92af5a677
randiltennakoon <randil2016al@gmail.com>
2021-12-27 16:56:42 +0530

    Initial Commit

---
+++ MyToDo/MyToDoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyToDoApp.swift
+//  MyToDo
+//
+//  Created by Randil Tennakoon on 2021-12-27.
+//
+
+import SwiftUI
+
+@main
+struct MyToDoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
